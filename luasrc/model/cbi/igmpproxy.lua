bb3cb4d7ab89d54aff7414f0f6662c84efd89292
xiaren2 <68859141+xiaren2@users.noreply.github.com>
2025-10-27 22:03:12 +0800

    Update igmpproxy.lua

--- luasrc/model/cbi/igmpproxy.lua
+++ luasrc/model/cbi/igmpproxy.lua
@@ -13,21 +13,19 @@ end
 
 -- 获取网络接口
 local networks = {}
-uci:foreach("network", "interface",
-    function(section)
-        if section[".name"] ~= "loopback" then
-            networks[#networks + 1] = section[".name"]
-        end
+uci:foreach("network", "interface", function(section)
+    if section[".name"] ~= "loopback" then
+        networks[#networks + 1] = section[".name"]
     end
-)
+end)
 
 -- 获取防火墙区域
 local zones = {}
-uci:foreach("firewall", "zone",
-    function(section)
+uci:foreach("firewall", "zone", function(section)
+    if section.name then
         zones[#zones + 1] = section.name
     end
-)
+end)
 
 m = Map("igmpproxy", translate("IGMP Proxy"), translate("Configure IGMP Proxy for multicast forwarding."))
 
@@ -35,11 +33,17 @@ m = Map("igmpproxy", translate("IGMP Proxy"), translate("Configure IGMP Proxy fo
 s = m:section(TypedSection, "igmpproxy", translate("General Settings"))
 s.anonymous = true
 
--- 只保留 quickleave 和 verbose 选项
+-- 启用开关
+o = s:option(Flag, "enabled", translate("Enable IGMP Proxy"))
+o.default = 1
+o.rmempty = false
+
+-- Quick Leave
 o = s:option(Flag, "quickleave", translate("Quick Leave"))
 o.default = 1
 o.rmempty = false
 
+-- Verbose 级别
 o = s:option(ListValue, "verbose", translate("Verbose Level"))
 o:value("0", translate("None"))
 o:value("1", translate("Minimal"))
@@ -54,25 +58,67 @@ s2.addremove = true
 s2.template = "cbi/tblsection"
 s2.anonymous = false
 
+-- 网络接口
 o = s2:option(ListValue, "network", translate("Network"))
+o:value("", translate("Not specified"))      -- 未指定
 for _, net in ipairs(networks) do
     o:value(net)
 end
-o.rmempty = false
+o:value("_custom_", translate("Custom..."))  -- 自定义选项
+o.rmempty = true
+function o.write(self, section, value)
+    if value == "_custom_" then
+        local custom = luci.http.formvalue("cbid.igmpproxy." .. section .. ".network_custom")
+        if custom and #custom > 0 then
+            uci:set("igmpproxy", section, "network", custom)
+        end
+    else
+        Value.write(self, section, value)
+    end
+end
+function o.cfgvalue(self, section)
+    local val = uci:get("igmpproxy", section, "network")
+    if val and not (val == "" or val == "_custom_") and not table.contains(networks, val) then
+        self.default = "_custom_"
+    end
+    return val
+end
 
+-- Zone 区域
 o = s2:option(ListValue, "zone", translate("Zone"))
+o:value("", translate("Not specified"))      -- 未指定
 for _, zone in ipairs(zones) do
     o:value(zone)
 end
+o:value("_custom_", translate("Custom..."))  -- 自定义选项
 o.rmempty = true
+function o.write(self, section, value)
+    if value == "_custom_" then
+        local custom = luci.http.formvalue("cbid.igmpproxy." .. section .. ".zone_custom")
+        if custom and #custom > 0 then
+            uci:set("igmpproxy", section, "zone", custom)
+        end
+    else
+        Value.write(self, section, value)
+    end
+end
+function o.cfgvalue(self, section)
+    local val = uci:get("igmpproxy", section, "zone")
+    if val and not (val == "" or val == "_custom_") and not table.contains(zones, val) then
+        self.default = "_custom_"
+    end
+    return val
+end
 
+-- 方向
 o = s2:option(ListValue, "direction", translate("Direction"))
 o:value("upstream", translate("Upstream"))
 o:value("downstream", translate("Downstream"))
 o.default = "downstream"
 
+-- Alt Network
 o = s2:option(DynamicList, "altnet", translate("Alt Network"))
-o.placeholder = "224.0.0.0/4"
+o.placeholder = "10.0.0.0/8"
 o.rmempty = true
 o.description = translate("Only valid for upstream interface. You can add multiple multicast ranges here.")
 
