fd9aaa857de480c0107c236f3bab54dbb65f24d4
xiaren2 <68859141+xiaren2@users.noreply.github.com>
2025-10-28 14:52:27 +0800

    Update igmpproxy.lua

--- luasrc/model/cbi/igmpproxy.lua
+++ luasrc/model/cbi/igmpproxy.lua
@@ -1,28 +1,6 @@
 local m, s, o
-local fs = require "nixio.fs"
 local uci = require "luci.model.uci".cursor()
-
--- 兼容 table.contains() 函数（新版 Luci/ucode 环境无此函数）
-if not table.contains then
-    function table.contains(tbl, val)
-        if not tbl or type(tbl) ~= "table" then return false end
-        for _, v in ipairs(tbl) do
-            if v == val then
-                return true
-            end
-        end
-        return false
-    end
-end
-
--- 确保配置文件存在并设置默认值
-if not uci:get_first("igmpproxy", "igmpproxy") then
-    uci:section("igmpproxy", "igmpproxy", nil, {
-        quickleave = "1",
-        verbose = "1"
-    })
-    uci:commit("igmpproxy")
-end
+local net = require "luci.model.network".init()
 
 m = Map("igmpproxy", "IGMP代理设置", "配置IGMP代理以实现组播转发")
 
@@ -30,13 +8,10 @@ m = Map("igmpproxy", "IGMP代理设置", "配置IGMP代理以实现组播转发"
 s = m:section(TypedSection, "igmpproxy", "常规设置")
 s.anonymous = true
 
--- Quick Leave
 o = s:option(Flag, "quickleave", "快速离开")
 o.default = 1
 o.rmempty = false
-o.description = "启用快速离开功能，减少离开延迟"
 
--- Verbose 级别
 o = s:option(ListValue, "verbose", "日志级别")
 o:value("0", "无")
 o:value("1", "最小")
@@ -51,19 +26,68 @@ s2.addremove = true
 s2.anonymous = false
 s2.template = "cbi/tblsection"
 
--- 使用OpenWrt官方风格的网络接口选择
-o = s2:option(Value, "network", "网络接口")
-o.template = "cbi/network_netlist"
-o.nocreate = true
-o.rmempty = false
+-- 自定义接口选择器
+o = s2:option(ListValue, "network", "网络接口")
+o:value("", "-- 请选择 --")
 
--- 使用OpenWrt官方风格的防火墙区域选择
+-- 获取所有网络设备并按类型分类
+local devices = net:get_devices()
+local ifaces = net:get_interfaces()
+
+-- 按类型排序和显示
+local types = {
+    bridge = "桥接",
+    ether = "以太网适配器",
+    tunnel = "隧道接口",
+    wireless = "无线网络",
+    alias = "接口别名"
+}
+
+-- 收集所有接口信息
+local interfaces = {}
+for _, dev in ipairs(devices) do
+    local iface = dev:get_interface()
+    if iface then
+        interfaces[iface:name()] = {
+            type = dev:type(),
+            dev = dev:name(),
+            iface = iface,
+            description = iface:get_i18n()
+        }
+    end
+end
+
+-- 按类型分组
+local grouped = {}
+for name, info in pairs(interfaces) do
+    local typ = info.type
+    if not grouped[typ] then grouped[typ] = {} end
+    table.insert(grouped[typ], info)
+end
+
+-- 添加接口到选项
+for typ, tname in pairs(types) do
+    if grouped[typ] then
+        o:value("", "--- "..tname.." ---")
+        for _, info in ipairs(grouped[typ]) do
+            local label = string.format("%s: %s", tname, info.dev)
+            if info.iface:is_bridge() then
+                label = label .. " (".. table.concat(info.iface:get_networks(), ", ") ..")"
+            elseif info.description ~= info.dev then
+                label = label .. " (".. info.description ..")"
+            end
+            o:value(info.dev, label)
+        end
+    end
+end
+
+-- 防火墙区域选择（保持官方风格）
 o = s2:option(Value, "zone", "防火墙区域")
 o.template = "cbi/firewall_zonelist"
 o.nocreate = true
 o.rmempty = true
 
--- 方向
+-- 方向选择
 o = s2:option(ListValue, "direction", "方向")
 o:value("upstream", "上行")
 o:value("downstream", "下行")
@@ -71,10 +95,9 @@ o.default = "downstream"
 o.rmempty = false
 
 -- Alt Network
-o = s2:option(DynamicList, "altnet", "放行网段")
+o = s2:option(DynamicList, "altnet", "放行网络段")
 o.placeholder = "例如: 10.0.0.0/8"
 o.rmempty = true
 o:depends("direction", "upstream")
-o.description = "仅对上行接口有效。可以添加多个组播地址/中间地址范围。"
 
 return m
