60503ec44dcfcc1c7d9b7d81c6a003ef18d181a9
xiaren2 <68859141+xiaren2@users.noreply.github.com>
2025-10-28 14:27:32 +0800

    Update igmpproxy.lua

--- luasrc/model/cbi/igmpproxy.lua
+++ luasrc/model/cbi/igmpproxy.lua
@@ -1,10 +1,8 @@
 local m, s, o
-local uci = require "luci.model.uci".cursor()
 local fs = require "nixio.fs"
-local network = require "luci.model.network".init()
-local firewall = require "luci.model.firewall".init(uci)
+local uci = require "luci.model.uci".cursor()
 
--- 兼容 table.contains()
+-- 兼容 table.contains() 函数（新版 Luci/ucode 环境无此函数）
 if not table.contains then
     function table.contains(tbl, val)
         if not tbl or type(tbl) ~= "table" then return false end
@@ -26,62 +24,57 @@ if not uci:get_first("igmpproxy", "igmpproxy") then
     uci:commit("igmpproxy")
 end
 
--- 主页面
-m = Map("igmpproxy", translate("IGMP Proxy"), translate("Configure IGMP Proxy for multicast forwarding."))
+m = Map("igmpproxy", "IGMP代理设置", "配置IGMP代理以实现组播转发")
 
 -- 常规设置
-s = m:section(TypedSection, "igmpproxy", translate("General Settings"))
+s = m:section(TypedSection, "igmpproxy", "常规设置")
 s.anonymous = true
 
 -- Quick Leave
-o = s:option(Flag, "quickleave", translate("Quick Leave"))
+o = s:option(Flag, "quickleave", "快速离开")
 o.default = 1
 o.rmempty = false
+o.description = "启用快速离开功能，减少离开延迟"
 
 -- Verbose 级别
-o = s:option(ListValue, "verbose", translate("Verbose Level"))
-o:value("0", translate("None"))
-o:value("1", translate("Minimal"))
-o:value("2", translate("More"))
-o:value("3", translate("Maximum"))
+o = s:option(ListValue, "verbose", "日志级别")
+o:value("0", "无")
+o:value("1", "最小")
+o:value("2", "中等")
+o:value("3", "详细")
 o.default = "1"
 o.rmempty = false
 
 -- 接口设置
-s2 = m:section(TypedSection, "phyint", translate("Interfaces"))
+s2 = m:section(TypedSection, "phyint", "接口配置")
 s2.addremove = true
-s2.template = "cbi/tblsection"
 s2.anonymous = false
+s2.template = "cbi/tblsection"
 
--- 网络接口
-o = s2:option(ListValue, "network", translate("Network"))
-o:value("", translate("Not specified"))  -- 未指定
-for _, iface in ipairs(network:get_interfaces()) do
-    local ifname = iface:name()
-    local proto = iface:get("proto") or "?"
-    local desc = string.format("%s (%s)", ifname, proto)
-    o:value(ifname, desc)
-end
-o.rmempty = true
+-- 使用OpenWrt官方风格的网络接口选择
+o = s2:option(Value, "network", "网络接口")
+o.template = "cbi/network_netlist"
+o.nocreate = true
+o.rmempty = false
 
--- 防火墙区域
-o = s2:option(ListValue, "zone", translate("Zone"))
-o:value("", translate("Not specified"))  -- 未指定
-for _, z in ipairs(firewall:get_zones()) do
-    o:value(z.name, z.name)
-end
+-- 使用OpenWrt官方风格的防火墙区域选择
+o = s2:option(Value, "zone", "防火墙区域")
+o.template = "cbi/firewall_zonelist"
+o.nocreate = true
 o.rmempty = true
 
 -- 方向
-o = s2:option(ListValue, "direction", translate("Direction"))
-o:value("upstream", translate("Upstream"))
-o:value("downstream", translate("Downstream"))
+o = s2:option(ListValue, "direction", "方向")
+o:value("upstream", "上行")
+o:value("downstream", "下行")
 o.default = "downstream"
+o.rmempty = false
 
 -- Alt Network
-o = s2:option(DynamicList, "altnet", translate("Alt Network"))
-o.placeholder = "10.0.0.0/8"
+o = s2:option(DynamicList, "altnet", "放行网段")
+o.placeholder = "例如: 10.0.0.0/8"
 o.rmempty = true
-o.description = translate("Only valid for upstream interface. You can add multiple multicast ranges here.")
+o:depends("direction", "upstream")
+o.description = "仅对上行接口有效。可以添加多个组播地址/中间地址范围。"
 
 return m
